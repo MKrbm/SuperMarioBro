8dd39a2925043aa9e01d4db4bcef17017a1a0b9d
murota keisuke <murotakeisuke@murotas-MacBook-Pro.local>
2020-01-04 02:03:15 +0900

    added function within test.py

--- test.py
+++ test.py
@@ -1,24 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
-plt.style.use('seaborn-pastel')
 
+class area:
+	def __init__(self,r):
+		self.r = r
 
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 4), ylim=(-2, 2))
-line, = ax.plot([], [], lw=3)
-
-def init():
-    line.set_data([], [])
-    return line,
-def animate(i):
-    x = np.linspace(0, 4, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
-    line.set_data(x, y)
-    return line,
-
-anim = FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=20, blit=True)
-
-
-anim.save('sine_wave.gif', writer='imagemagick')
+	def area(self):
+		return np.pi**2*self.r
